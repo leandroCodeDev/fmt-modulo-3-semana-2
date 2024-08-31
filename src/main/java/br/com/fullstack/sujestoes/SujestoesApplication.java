fd9b9637af44219e76d10f4a234c9cddc68b9dc0
Leandro Dias <lvd@certi.org.br>
2024-08-31 18:44:37 -0300

    iniciado projeto

---
+++ src/main/java/br/com/fullstack/sujestoes/SujestoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.sujestoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SujestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SujestoesApplication.class, args);
+	}
+
+}
